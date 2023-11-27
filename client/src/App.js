09f92a3ce44af0cd12879f37a9ebf3dbcc669396
Hariom Yadav <hariom.yadav@astridinfotech.com>
2023-11-27 22:26:28 +0530

    Constats added

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-import {useEffect, useState} from 'react';
-import axios from 'axios';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Constants from "./utils/Constants";
 
 function App() {
-  const [state, setState] = useState([])
-  const getQuotes = async ()=>{
+  const [state, setState] = useState([]);
+  const getQuotes = async () => {
     try {
-      const res = await axios({url: 'http://localhost:8001/api/a1/user/quotes', 
-      method: 'get',
-      });
-      if(res.status === 200){
+      const url = Constants.BASE_URL + Constants.Paths.USER_QUOTES;
+      const res = await axios({ url, method: "get" });
+      if (res.status === 200) {
         setState(res.data.getQuotes);
       }
-      console.log(res)
+      console.log(res);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-  useEffect(()=>{
-    getQuotes()
-  },[])
+  };
+  useEffect(() => {
+    getQuotes();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        {
-          state?.map(({text, auther})=> <p>{text} <sub> __{auther}</sub></p>)
-        }
-        
+        {state?.map(({ text, auther }) => (
+          <p>
+            {text} <sub> __{auther}</sub>
+          </p>
+        ))}
       </header>
     </div>
   );
---
+++ client/src/utils/Constants.jsx
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    BASE_URL: 'http://localhost:8001/api/a1/',
+    Paths: {
+        USER_QUOTES: 'user/quotes',
+    }
+})
