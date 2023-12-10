67f07df881fe3c271ae6fe51d317e7601e3ded54
Hariom Yadav <hariomask0111@gmail.com>
2023-12-10 13:05:49 +0530

    add one line

---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../../components/navbars/Navbar';
+import Footer from '../../components/footers/Footer';
+
+const HomePage = () => {
+    return (
+        <>
+           <Navbar /> 
+           <div className='width-100 h-96'></div>
+           <div className='width-100 h-96'></div>
+           <Footer />
+        </>
+    );
+};
+
+export default HomePage;
